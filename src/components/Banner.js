4112e106113b58fe87a523bbc80b21841e0c03b6
LazyCoder07 <gaganagarwal700@gmail.com>
2021-02-08 18:44:38 +0530

    Updated Navbar and Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -4,11 +4,9 @@ import logo from '../assets/zense_logo.png';
 import '../static/Banner.css'
 function Banner() {
     return (
-        <section>
+        <section className="main_banner">
            <img className="main_banner_logo" src = {logo} />
-           <h2 className="main_title">
-               Lorem Ipsum.
-           </h2>
+           <h2 className="main_title">LOREM IPSUM.</h2>
            <p>
            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
            </p>
--- src/components/MyNavbar.js
+++ src/components/MyNavbar.js
@@ -1,28 +1,30 @@
-import {Nav,  Navbar} from 'react-bootstrap';
+import {Nav,  Navbar, Container} from 'react-bootstrap';
 import '../static/Navbar.css'
 import logo from '../assets/zense_logo.png';
 function MyNavbar() {
   return(
-<Navbar sticky="top" bg = "navbar">
+<Navbar collapseOnSelect sticky="top" bg = "navbar" expand="lg" >
+  <Container>
     <Navbar.Brand className="nav_brand" href="/">
       <img
         src= {logo}
-        height="50"
+        height="60"
         className="d-inline-block align-top"
         alt="React Bootstrap logo"
       /> 
     </Navbar.Brand>
-
-    <Nav className = "ml-auto">
-      <Nav.Link className = "nav-link" href="projects"><span>Projects</span></Nav.Link>
-      <Nav.Link className = "nav-link" href="news"><span>News</span></Nav.Link>
-      <Nav.Link className = "nav-link" href="blog"><span>Blog</span></Nav.Link>
-      <Nav.Link className = "nav-link" href="people"><span>People</span></Nav.Link>
-      <Nav.Link className = "nav-link" href="about"><span>About</span></Nav.Link>
-      <Nav.Link className = "nav-link" href="contact"><span>Contact</span></Nav.Link>
-    </Nav>
-    
-
+    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+    <Navbar.Collapse id="responsive-navbar-nav">
+      <Nav className = "ml-auto">
+        <Nav.Link className = "nav-link" href="projects"><span>Projects</span></Nav.Link>
+        <Nav.Link className = "nav-link" href="news"><span>News</span></Nav.Link>
+        <Nav.Link className = "nav-link" href="blog"><span>Blog</span></Nav.Link>
+        <Nav.Link className = "nav-link" href="people"><span>People</span></Nav.Link>
+        <Nav.Link className = "nav-link" href="about"><span>About</span></Nav.Link>
+        <Nav.Link className = "nav-link" href="contact"><span>Contact</span></Nav.Link>
+      </Nav>
+    </Navbar.Collapse>
+    </Container>
   </Navbar>)
 }
 export default MyNavbar
