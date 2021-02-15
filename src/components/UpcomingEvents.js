f76059a97b5cd21486562b657b4f39ec7dd85cdc
jaggu21 <Siva.jagadesh@iiitb.org>
2021-02-15 20:53:07 +0530

    Updated Upcoming Events

--- src/components/UpcomingEvents.js
+++ src/components/UpcomingEvents.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {Row,Col,Container} from 'react-bootstrap';
 import '../static/UpcomingEvents.css';
 import UpcomingEventsCard from './UpcomingEventsCard';
+import Carousel from 'react-elastic-carousel';
 
 const Data = [
   {
+    id:0,
     Title:'CTF',
     Subtitle:'20 March 2021',
     Text:'Sunt exercitation eiusmod enim cupidatat occaecat labore. Ut Lorem adipisicing consequat Lorem cillum. Esse deserunt proident deserunt ut commodo',
@@ -12,6 +14,15 @@ const Data = [
     LinkText:'Know More'
   },
   {
+    id:1,
+    Title:'CTF',
+    Subtitle:'20 March 2021',
+    Text:'Sunt exercitation eiusmod enim cupidatat occaecat labore. Ut Lorem adipisicing consequat Lorem cillum. Esse deserunt proident deserunt ut commodo',
+    Link:'#',
+    LinkText:'Know More'
+  },
+  {
+    id:2,
     Title:'CTF',
     Subtitle:'20 March 2021',
     Text:'Sunt exercitation eiusmod enim cupidatat occaecat labore. Ut Lorem adipisicing consequat Lorem cillum. Esse deserunt proident deserunt ut commodo',
@@ -23,12 +34,23 @@ const Data = [
 function UpcomingEvents() {
   return (
     <div style={{display: 'flex', flexDirection: 'row'}}>
-      <Container fluid className="upcoming-events-container">
+      <Carousel>
+        <Container fluid className="upcoming-events-container">
         <Row className="justify-content-md-center">
           <Col > <UpcomingEventsCard Data={Data[0]}/></Col>
           <Col > <UpcomingEventsCard Data={Data[1]}/></Col>
+          <Col > <UpcomingEventsCard Data={Data[2]}/></Col>
         </Row>
-      </Container>
+       </Container>
+       <Container fluid className="upcoming-events-container">
+        <Row className="justify-content-md-center">
+          <Col > <UpcomingEventsCard Data={Data[0]}/></Col>
+          <Col > <UpcomingEventsCard Data={Data[1]}/></Col>
+          <Col > <UpcomingEventsCard Data={Data[2]}/></Col>
+        </Row>
+       </Container>
+      </Carousel>
+      
     </div>
     );
 }
