77e67f1804e63e2ee447b0b8b9f33ec8fdf4ac2d
LazyCoder07 <gaganagarwal700@gmail.com>
2021-02-17 02:57:09 +0530

    Replace HashLink with simple js scroll

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,9 +2,12 @@ import React from 'react'
 import logo from '../assets/zense_logo.png';
 import '../static/Banner.css'
 import downArrow from '../assets/arrow-down-circle.png'
-import { HashLink } from 'react-router-hash-link';
-
+import {Link} from 'react-router-dom'
 function Banner() {
+    const executeScroll = () => {
+        var element = document.getElementById("projects");
+        element.scrollIntoView();
+    }
     return (
         <section className="main_banner">
            <img className="main_banner_logo" src = {logo} />
@@ -13,7 +16,7 @@ function Banner() {
            We are a community of developers run by the students of IIIT Bangalore <br />
            We work to build things and sometimes break them too. We organise events and promote open source culture at IIITB. We are tech enthusiasts and are looking to work with those of our kind.
            </p>
-           <HashLink to="#projects" className="circle-down-arrow"><img src= {downArrow} className="arrow-image" /></HashLink>
+           <Link to="#projects" className="circle-down-arrow" onClick={executeScroll}><img src= {downArrow} className="arrow-image" /></Link>
         </section>
     )
 }
