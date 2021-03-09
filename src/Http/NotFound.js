f1ce32f83e50bc4691561491a7e96e5b3ae48820
divyamagwl <agrawaldivyam2001@gmail.com>
2021-03-09 23:47:45 +0530

    Minor css updates

--- src/Http/NotFound.js
+++ src/Http/NotFound.js
@@ -5,7 +5,7 @@ import image404 from '../assets/404.png'
 function NotFound() {
 	return(
 		<div className='container'>
-			<img className='http-err-msg' src={image404}></img>
+			<img className='http-err-img' src={image404}></img>
 			<div className='http-err'>
 				Uh oh! You seem to be lost
 				<a className='home-button' href='/'>
