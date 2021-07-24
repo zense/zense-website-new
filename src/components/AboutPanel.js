fa22c6fc52b708a0b42ba57fd12554e0c4e1cd45
GSri30 <g.sriharsha@iiitb.org>
2021-07-25 01:58:57 +0530

    about-us : altered panels

--- src/components/AboutPanel.js
+++ src/components/AboutPanel.js
@@ -1,27 +1,51 @@
 import React from 'react'
 import '../static/About.css'
 
-function AboutPanel(props) {
+function AboutPanelRight(props) {
 	const data = props.data.data;
 	return(
 		
-			<div className="about_panel container">
+			<div className="about_panel about_panel_right container">
 				<div className="row row-centered pos">
-					<div className="about_info col-lg-6 col-md-12 col-xs-12 col-centered">
+					<div className="about_info about_info_right col-lg-6 col-md-12 col-xs-12 col-centered">
 						
 						<h4 className="about_title">{data.title}</h4>
 						<div className="about_text"> 
 							{data.text}
 						</div>
 					</div>
-					<div className="about_picture col-lg-6 col-md-12 col-xs-12 col-centered">
-						<img className="about_picture col-lg-12 col-md-12 col-xs-12 col-centered" src={data.imgsrc} alt={data.imgalt} />
+					<div className="about_picture about_picture_right col-lg-6 col-md-12 col-xs-12 col-centered">
+						<img className="about_picture_right col-lg-12 col-md-12 col-xs-12 col-centered" src={data.imgsrc} alt={data.imgalt} />
 					</div>
 				</div>
 			</div>
 		
-		
 		)
 }
 
-export default AboutPanel
+function AboutPanelLeft(props) {
+	const data = props.data.data;
+	return(
+		
+			<div className="about_panel about_panel_left container">
+				<div className="row row-centered pos">
+					<div className="about_picture about_picture_left col-lg-6 col-md-12 col-xs-12">
+						<img className="about_picture_left col-lg-12 col-md-12 col-xs-12" src={data.imgsrc} alt={data.imgalt} />
+					</div>
+					<div className="about_info about_info_left col-lg-6 col-md-12 col-xs-12 col-centered">
+						
+						<h4 className="about_title">{data.title}</h4>
+						<div className="about_text"> 
+							{data.text}
+						</div>
+					</div>
+				</div>
+			</div>
+
+		)
+}
+
+export {
+	AboutPanelRight,
+	AboutPanelLeft
+}
